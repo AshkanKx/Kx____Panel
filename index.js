05be56bf5793ea9cb324d79173b723ed6a0f3929
AshkanKx <ashkantajikov@gmail.com>
2026-08-14 18:10:05 +0330

    Initialize Telegraf bot with start command

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { Telegraf } = require("telegraf");
 
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+if (!BOT_TOKEN) {
+  throw new Error("BOT_TOKEN is not set");
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply("🔥 به ASHKAN KX خوش اومدی!");
+});
+
+bot.launch();
+
+console.log("KX Panel Bot is running...");
