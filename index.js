aafce9c876372b71db3de6aa8a042174b7e108f7
AshkanKx <ashkantajikov@gmail.com>
2026-08-15 08:29:14 +0330

    Implement HTTP server for KX Panel Bot
    Added an HTTP server to respond with a status message.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require("telegraf");
+const http = require("http");
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
@@ -10,6 +11,18 @@ const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) => {
   ctx.reply("🔥 به ASHKAN KX خوش اومدی!");
+});
+
+// HTTP server برای Render
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("KX Panel Bot is alive 🔥");
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`HTTP server running on port ${PORT}`);
 });
 
 bot.launch();
